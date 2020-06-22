92daafa60ce040602cd4a7ed2a0f93707ea8c636
bansal-rahul <rahul@proptiger.com>
2020-06-22 17:41:00 +0530

    adding headers in response

--- main.go
+++ main.go
@@ -9,12 +9,33 @@ const port = ":4000"
 
 // handler func for home
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from textbin"))
+}
+
+// handler func to show a recorded snippet
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Show a particualr snippet..."))
+}
+
+// handler func to crate a new snippet
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	// start and listen to server
 	log.Println("Starting server on", port)
