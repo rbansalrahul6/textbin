a746223e50b29901468f23f8810d50f5ef6210c8
bansal-rahul <rahul@proptiger.com>
2020-06-24 09:55:03 +0530

    inserting snippet in db

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -49,5 +49,16 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create a new snippet..."))
+	// w.Write([]byte("Create a new snippet..."))
+	// dummy data; TODO: remove later
+	title := "0 snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -13,9 +13,19 @@ type SnippetModel struct {
 
 /* DB methods for our models*/
 
-// Insert new snippet into DB
+// Insert new snippet into DB-> returns id
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	query := `INSERT INTO snippets (title, content, created, expires)
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+	result, err := m.DB.Exec(query, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
 }
 
 // Get snippet by ID
