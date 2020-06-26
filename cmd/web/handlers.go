f92a5348f75f9488b265b551ce75ae48ef6d4e06
bansal-rahul <rahul@proptiger.com>
2020-06-26 16:12:27 +0530

    code cleanup

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,11 +11,6 @@ import (
 
 // handler func for home
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	// if r.URL.Path != "/" {
-	// 	app.notFound(w)
-	// 	return
-	// }
-
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -50,53 +45,21 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 // handler func to crate a new snippet
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	// if r.Method != "POST" {
-	// 	w.Header().Set("Allow", "POST")
-	// 	app.clientError(w, http.StatusMethodNotAllowed)
-	// 	return
-	// }
-	// w.Write([]byte("Create a new snippet..."))
 	err := r.ParseForm()
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	// title := r.PostForm.Get("title")
-	// content := r.PostForm.Get("content")
-	// expires := r.PostForm.Get("expires")
 	form := forms.New(r.PostForm)
 	form.Required("title", "content", "expires")
 	form.MaxLength("title", 100)
 	form.PermittedValues("expires", "365", "7", "1")
 
-	// // user input validation
-	// errors := make(map[string]string)
-	// // check non-null & max len constraint
-	// if strings.TrimSpace(title) == "" {
-	// 	errors["title"] = "This field cannot be blank"
-	// } else if utf8.RuneCountInString(title) > 100 {
-	// 	errors["title"] = "This field is too long (maximum is 100 characters)"
-	// }
-
-	// // validations for content field
-	// if strings.TrimSpace(content) == "" {
-	// 	errors["content"] = "This field cannot be blank"
-	// }
-
-	// // validations for expires field
-	// if strings.TrimSpace(expires) == "" {
-	// 	errors["expires"] = "This field cannot be blank"
-	// } else if expires != "1" && expires != "7" && expires != "365" {
-	// 	errors["expires"] = "This field is invalid"
-	// }
-
 	if !form.Valid() {
-		// TODO: re-display form with errors
 		app.render(w, r, "create.page.tmpl", &templateData{
 			Form: form,
 		})
-		// fmt.Fprint(w, errors)
 		return
 	}
 
